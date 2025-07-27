16242f3fd9013dc078db18b2b138458335c469e8
Joaomacosdev <joamarcosdev@gmail.com>
2025-07-27 20:59:01 -0300

    Iniciando o projeto

---
+++ payflux/src/test/java/br/com/joao/payflux/PayfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joao.payflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
